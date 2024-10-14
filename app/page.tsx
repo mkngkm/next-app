729a91ad0cc63227974ea909a7f0e4967c6dfb86
mkngkm <mikang1031@gmail.com>
2024-10-14 13:28:12 +0900

    비로그인시 리디렉션

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+// app/page.tsx
 import Link from 'next/link';
-export default function HomePage() {
-  const apiUrl = process.env.LOCAL_API_URL;
-  const apikey = process.env.LOCAL_API_KEY;
+
+const HomePage = () => {
   return (
     <div>
-      <p>API URL : {apiUrl}</p>
-      <p>API KEY : {apikey}</p>
+      <h1>Welcome to the Home Page</h1>
+      <Link href='/todos'>Go to Todos</Link>
     </div>
   );
-}
+};
+
+export default HomePage;
---
+++ middleware.ts
@@ -0,0 +1,22 @@
+// middleware.ts
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+
+export async function middleware(req: any) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  // 사용자가 로그인하지 않았다면 /login으로 리디렉션
+  if (!token && req.nextUrl.pathname !== '/login') {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  // 사용자가 로그인한 경우, /login 페이지에 접근하면 홈으로 리디렉션
+  if (token && req.nextUrl.pathname === '/login') {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+}
+
+// 적용할 경로를 설정 (모든 경로에 대해 미들웨어 적용)
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
